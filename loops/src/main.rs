727b5ff5847d30cb7dac8707b6eda6a7b3c17c7e
kenichi_shintome <kshintome@say-tech.co.jp>
2023-11-06 12:23:04 +0900

    3.5制御フローのループを追加

---
+++ loops/src/main.rs
@@ -0,0 +1,58 @@
+fn main() {
+    loop_label();
+    while_loop();
+    for_loop();
+    for_loop_rev();
+}
+
+//ループでラベルが使える
+fn loop_label() {
+    let mut count = 0;
+
+    'counting_up: loop {
+        println!("count = {}", count);
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {}", remaining);
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End Count = {}", count);
+}
+
+//whileでのループ
+fn while_loop() {
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}!", number);
+        number -= 1;
+    }
+    println!("LIFTOFF!!!");
+}
+
+//forでの要素ループ
+fn for_loop() {
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {}", element);
+    }
+}
+
+//forでの逆要素ループ
+fn for_loop_rev() {
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
+}
